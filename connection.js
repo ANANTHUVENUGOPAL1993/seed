46e5a10e695aa35ee8122a504a10cdeb56dd6324
ananthuvenugopal1993 <ananthuvenugopal3@gmail.com>
2022-05-26 11:46:05 +0530

    user

---
+++ connection.js
@@ -0,0 +1,9 @@
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
---
+++ seed.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+require('dotenv').config();
+
+const Seed=require('./model')
+
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
+
+
+const seedData=[  
+    {name:"superadmin", email:"superadmin@gmail.com",password:'fnsjdnfsjkd99u9u0912'},  
+    {name:"admin", email:"admin@gmail.com",password:'fnsjdnfsjkd99u9u0912'}  
+] 
+
+
+const seedDB=async ()=>{
+    await Seed.deleteMany({})
+    await Seed.insertMany(seedData)
+}
+
+
+
+seedDB().then(()=>{
+    mongoose.connection.close();
+})
